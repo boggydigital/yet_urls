f1f0ad2fe6eaca5f6559c1f792ddcf2f070042d7
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-07-28 00:48:28 -0700

    Adding player url helper

---
+++ player_url.go
@@ -0,0 +1,13 @@
+package yt_urls
+
+import "net/url"
+
+func PlayerUrl(path string) *url.URL {
+	playerUrl := &url.URL{
+		Scheme: httpsScheme,
+		Host:   youtubeWwwHost,
+		Path:   path,
+	}
+
+	return playerUrl
+}
